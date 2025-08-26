7dbdc24bfe16fa1f46f07e7bd9407292f10e14f5
Daniel Figueredo <danielfsa2007@gmail.com>
2025-08-26 20:08:45 -0300

    Calculador de IMC
    Desenvolvi um calculador de IMC utilizando C

---
+++ calcular-IMC/imc.c
@@ -0,0 +1,22 @@
+/*
+	Name: Calculador de IMC
+	Author: Daniel Figueredo
+	Date: 26/08/25 10:57
+	Description: Programa para calcular o imc de uma pessoa
+*/
+#include <stdio.h>
+#include <locale.h>
+main()
+{
+	setlocale(LC_ALL,"Portuguese");
+	float alt, peso, imc;
+	alt= 0; peso = 0; imc = 0;
+	puts("Programa para calcular o imc de uma pessoa");
+	printf("Digite sua altura:");
+	scanf("%f" , &alt);
+	printf("Digite seu peso:");
+	scanf("%f", &peso);
+	imc = peso/(alt*alt);
+	printf("Seu imc é: %.4f", imc);
+	
+}
