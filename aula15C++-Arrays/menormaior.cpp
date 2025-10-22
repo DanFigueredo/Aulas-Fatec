9a37c9dbd3e36ee9261819b9c6a7ab91fc1d500b
Daniel Figueredo <danielfsa2007@gmail.com>
2025-10-22 19:47:07 -0300

    Treinando

---
+++ aula15C++-Arrays/menormaior.cpp
@@ -0,0 +1,37 @@
+/*
+Daniel Figueredo
+22/10/2025
+*/
+#include <stdio.h>
+int main(){
+    //Declarar variaveis 
+    int vet[8], maior, menor, i, tam;
+    maior = menor = i = 0;
+    tam = sizeof(vet)/sizeof(int);
+
+    printf("Digite 8 numeros: \n");
+
+  
+    for ( i = 0; i < tam; i++)
+    {
+        scanf("%d", &vet[i]);
+    }
+    for ( i = 0; i < tam; i++)
+    {
+        printf("%d |", vet[i]);
+    }
+    //Mostrar o maior
+    for ( i = 0; i < tam; i++)
+    {
+        maior = maior + vet[i];
+        if (vet[i]> maior)
+        {
+           vet[i] = maior;
+        }
+        
+        printf("%d", maior);
+    }
+    
+   
+}//fim do programa
+
