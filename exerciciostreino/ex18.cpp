adecd6e61f270e52d294d32b4e59ad2d482dbdc3
Daniel Figueredo <danielfsa2007@gmail.com>
2025-12-09 23:32:04 -0300

    Reforçando a base de algoritimo
    Programa que le numeros inteiros e identifica se é par ou impar apenas com logica, sem utilização do modulo(%).

---
+++ exerciciostreino/ex18.cpp
@@ -0,0 +1,19 @@
+/*
+Daniel Figueredo
+09/12/2025 11:21 PM
+Programa que verifica se o numero é par ou impar 
+sem usar o modulo
+*/
+#include <stdio.h>
+int main()
+{
+    int quociente, num, dobro;
+    printf("Digite um numero: ");
+    scanf("%d",&num);
+    quociente = num/2;
+    dobro = quociente*2;
+    if(dobro == num)
+        printf("%d e par",num);
+    else
+        printf("%d e impar", num);
+}//fim do programa
