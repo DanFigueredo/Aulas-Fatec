d24605459401076f0ce513d2b14a82aee44301d5
Daniel Figueredo <danielfsa2007@gmail.com>
2025-12-13 00:02:26 -0300

    programa que leia o sexo (M/F) e a altura de 50 pessoas.

--- exerciciostreino/ex26.cpp
+++ exerciciostreino/ex26.cpp
@@ -7,28 +7,30 @@ Programa que le 100 idades e armazena em um vetor e diga
 #include <stdio.h>
 int main()
 {
-    int idade[10];
+    int idade[100];
     int i = 0;
     float media = 0.0;
     int soma = 0;
     int menorIdade = 0;
     //Carregar o vetor
-    for ( i = 0; i < 10; i++)
+    for ( i = 0; i < 100; i++)
     {
         printf("Digite sua idade: ");
         scanf("%d",&idade[i]);
-        if (idade[i]> 18)
+        if (idade[i]<18)
         {
             menorIdade++;
         }
         
     }
-    for ( i = 0; i < 10; i++)
+    for ( i = 0; i < 100; i++)
     {
         soma = soma+idade[i];
 
     }
-    media = soma/i;
+    media = soma/100;
+
+    //Mostrar os resultados
 
     printf("A media das idades e: %.2f",media);
     printf("\n%d sao menores de idade",menorIdade);
