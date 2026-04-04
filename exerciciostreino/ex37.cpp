5e9c342b2a56b0406f1c3bd1ad04afe996732a51
Daniel Figueredo <danielfsa2007@gmail.com>
2026-04-03 21:11:31 -0300

    feat: adiciona função de contagem regressiva com ponteiros
    - recebe número inteiro no main e passa o endereço para a função
    - implementa contagemR para exibir contagem de n até -n
    - exercício de uso de ponteiros em funções em C

--- exerciciostreino/ex37.cpp
+++ exerciciostreino/ex37.cpp
@@ -7,38 +7,43 @@ Daniel Figueredo
 #include <stdio.h>
 
 //prototipção
-void verificarNum();
+void verificarNum(int *n);
 
 int main()
 {
-    verificarNum();
+    int num = 0;
+    // pedir e ler um numero
+    printf("\nDigite um numero positivo: ");
+    scanf("%d",&num);
+    verificarNum(&num);
     return 0;
 }//fim do programa
 
-void verificarNum()
+void verificarNum(int *n)
 {
   while (1)
   {
-    // pedir e ler um numero
-    int num = 0;
-    printf("\nDigite um numero positivo: ");
-    scanf("%d",&num);
+   
     //impor uma condição que se num for = 0 o programa acaba
-    if (num ==0 )
+    if (*n == 0 )
     {
+        
         printf("Programa encerrado");
         break;
-    }else if (num < 0) //outra condição (se num for menor que 0 pedir outro nume)
+    }else if (*n < 0) //outra condição (se num for menor que 0 pedir outro numero)
     {
-        printf("\nDigite um numero valido!!!!!!!!!\n");
-        continue;
-    }//fim da condição
+        puts("\nDigite um numero invalido!!!!!!!!!\n");
     
-    //imprimir o num até ele negativo, diminuindo  1 em 1, 
-    for (int i = num; i >= num * -1; i--)
-    {
-        printf(" %d ",i);    
-    }//fim do loop
+    }//fim da condição
+    else{
+          //imprimir o num até ele negativo, diminuindo  1 em 1, 
+        for (int i = *n; i >=*n * -1; i--)
+        {
+            printf(" %d ",i);    
+        }//fim do loop
+    }
+     printf("\nDigite um numero positivo: ");
+     scanf("%d", n);
        
   }//fim do loop infinito
 
---
+++ exerciciostreino/ex38.cpp
@@ -0,0 +1,23 @@
+/*
+Daniel Figueredo
+*/
+
+#include <stdio.h>
+void contagemR(int *n);
+
+int main()
+{
+    int num = 0;
+    printf("Digite um numero: "); scanf("%d",&num);
+    contagemR(&num);
+
+}
+
+void contagemR (int *n)
+{
+    
+    for (int i = *n; i >=*n * -1; i--)
+        {
+            printf(" %d ",i);    
+        }//fim do loop
+}
