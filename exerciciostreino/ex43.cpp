f8a67ca031665771191829fcb9da55e0e473eb2e
Daniel Figueredo <danielfsa2007@gmail.com>
2026-04-10 13:25:52 -0300

    feat: implementa manipulação de matriz e conversão para vetor
    feat: implementa manipulação de matriz e conversão para vetor
    
    - Implementa função para carregar matriz com dimensões definidas pelo usuário
    - Cria função para exibir matriz formatada
    - Desenvolve lógica para converter matriz em vetor unidimensional
    - Implementa função para exibição do vetor
    - Utiliza passagem de matriz como parâmetro com tamanho fixo de colunas
    - Código organizado em funções, seguindo boas práticas de modularização

---
+++ exerciciostreino/ex43.cpp
@@ -0,0 +1,117 @@
+/*
+Segunda Lista – P2
+1) Escreva um programa que faça a carga em uma matriz com a quantidade de linhas e
+colunas definida pelo usuário. A carga deve ser feita por meio de uma função. Após
+a carga, os elementos da matriz deverá ser passado para um vetor, em uma nova
+função. Ao final, tanto o vetor quanto a matriz devem sem impressos cada um por
+uma função.
+*/
+
+/*
+Daniel Figueredo 
+10/04/2026
+Programa que faça a carga em uma matriz com a quantidade de linhas e
+colunas definida pelo usuário, a carga deve ser feita por uma função.
+Após a carga, os elementos da matriz deverá ser passado para um vetor, em uma nova
+função. Ao final, tanto o vetor quanto a matriz devem sem impressos cada um por
+uma função.
+*/
+
+//importação:
+#include <stdio.h>
+
+//prototipação:
+void carregaMat();
+void mostrarMat(int , int ,int m[][100]);
+void carregarVet(int ,int ,int m[][100]);
+void mostrarVet(int *,int );
+
+//programa
+int main()
+{
+    
+    carregaMat();
+    return 0;
+}//fim do programa
+
+//funções:
+void carregaMat()
+{
+    //variaveis:
+    int lin = 0;
+    int col = 0;
+    int i = 0;
+    int j = 0;
+
+    //pedir as linhas e colunas da matriz pro usuario
+    printf("Digite a quantidade de linhas da matriz: ");
+    scanf("%d",&lin);
+
+    printf("Digite a quantidade de colunas da matriz: ");
+    scanf("%d",&col);
+
+    //criar matriz 
+    int mat[100][100];
+    //carregar a matriz
+    for ( i = 0; i < lin; i++)
+    {
+        for ( j = 0; j < col; j++)
+        {
+            printf("Digite os numeros da matriz: ");
+            scanf("%d",&mat[i][j]);
+        }//for interno
+        
+    }//for externo
+
+    //invoke da função pra mostrar a matriz:
+    mostrarMat(lin,col,mat);
+   
+}//fim da função que carrega a matriz
+
+//função pra mostrar a matriz
+void mostrarMat(int l, int c ,int m[][100])
+{
+    int i,j;
+    for ( i = 0; i < l; i++)
+    {
+        for ( j = 0; j < c; j++)
+        {
+            printf("[%d]",m[i][j]);
+        }
+        puts("\n");     
+    }
+    //invoke pra carregar o vetor
+    carregarVet(l,c,m);
+
+}//fim da função que mostra a matriz
+
+void carregarVet(int l,int c,int m[][100])
+{
+    int i,j;
+    int k = 0;
+    int vet[100];
+    for ( i = 0; i < l; i++)
+    {
+        for ( j = 0; j < c; j++)
+        {
+            vet[k] = m[i][j];
+            k++;
+        }
+        
+    }
+
+    //invoke pra mostrar o vetor
+    mostrarVet(vet,k);
+    
+}//fim da função que carrega o vetor 
+
+void mostrarVet(int *v,int t)
+{
+    int i = 0;
+    for (i = 0; i < t ; i++)
+    {
+        printf("%d | ",v[i]);
+    }
+    
+}
+
